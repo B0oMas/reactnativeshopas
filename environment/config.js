68d55bbea45c3f6768958856a08b809694a4515e
Lukas Anužis <b0omasino@gmail.com>
2021-10-08 20:23:12 +0300

    t

---
+++ environment/config.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSS6f2jCDKEKqnHLlEEAi0AxXJJLPQDPI",
+  authDomain: "reactnativeshopas-72926.firebaseapp.com",
+  databaseURL: "https://reactnativeshopas-72926-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "reactnativeshopas-72926",
+  storageBucket: "reactnativeshopas-72926.appspot.com",
+  messagingSenderId: "217174670229",
+  appId: "1:217174670229:web:cd243cf82f13c8da04949c",
+  measurementId: "G-2QZYZF6ZQH"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
